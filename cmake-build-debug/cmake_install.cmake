cd774f452f907faf26f4d958805cf29a198eeeca
islandboymv <mifzaal.scout@gmail.com>
2024-05-17 21:24:33 +0500

    Updated addPatient Function.

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/islandboy_mv/CLionProjects/untitled3
+# Install script for directory: /Users/islandboy_mv/CLionProjects/Patient-Billing-Systems
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/islandboy_mv/CLionProjects/untitled3/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/islandboy_mv/CLionProjects/Patient-Billing-Systems/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
